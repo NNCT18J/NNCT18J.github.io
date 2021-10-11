3ca56c40c81b6e40e63595fdbe6fddc838d62a88
JPig-Main <jumpei141107@gmail.com>
2021-10-11 21:31:06 +0900

    convert to tailwind-css (App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,32 @@ import Header from "./common/header/Header";
 import Footer from "./common/footer/Footer";
 import NotFound from "./common/notFound/NotFound";
 
-import "./css/app.css";
-
 const App = () => {
     const [site] = useState(data.site)
     const [pages] = useState(data.pages)
 
     return (
-        <div className="app">
+        <div className="flex flex-col w-screen min-h-screen">
             <Router>
-                <Header site={site} links={pages.map(page => {return {path: page.path, title: page.title}})} />
-                <main>
-                    <Switch className="page">
-                        {pages.map(page => 
-                            <Route exact 
-                                path={page.path} 
-                                render={() => 
-                                    <page.Component 
-                                        title={page.title} 
-                                        data={page.data} 
-                                    />
-                                }>
-                            </Route>
-                        )}
-                        <Route render={() => <NotFound />} />
-                    </Switch>
-                </main>
-                <Footer site={site} />
+                <Header
+                    site={site} links={pages.map(page => {return {path: page.path, title: page.title}})} 
+                    className="flex-initial"
+                />
+                <Switch className="flex-1 bg-gray-200">
+                    {pages.map(page => 
+                        <Route exact 
+                            path={page.path} 
+                            render={() => 
+                                <page.Component 
+                                    title={page.title} 
+                                    data={page.data} 
+                                />
+                            }>
+                        </Route>
+                    )}
+                    <Route render={() => <NotFound />} />
+                </Switch>
+                <Footer site={site} className="flex-initial" />
             </Router>
         </div>
     );
