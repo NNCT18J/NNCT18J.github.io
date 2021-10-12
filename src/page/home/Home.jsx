396957912e4ca5bcf7a3b36a9ba21b54321278ca
JPig-Main <jumpei141107@gmail.com>
2021-10-12 14:49:01 +0900

    少しアレンジ (home)

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -2,14 +2,20 @@ import './home.css'
 
 const Home = () => {
     return (
-        <div className="h-full text-center text-2xl">
-            <span className="strong">N</span>agano　
-            <span className="strong">N</span>ational　
-            <span className="strong">C</span>ollege　of　
-            <span className="strong">T</span>echnology　
-            20<span className="strong">18</span>　
-            <span className="strong">J</span>ouhou
-        </div >
+        <div className="mx-4 my-4">
+            <div className="flex">
+                <div className="flex-1"></div>
+                <div className="flex-inline p-8 mx-auto text-center text-2xl bg-gray-100 rounded-xl">
+                    <span className="strong">N</span>agano　
+                    <span className="strong">N</span>ational　
+                    <span className="strong">C</span>ollege　of　
+                    <span className="strong">T</span>echnology　
+                    20<span className="strong">18</span>　
+                    <span className="strong">J</span>ouhou
+                </div>
+                <div className="flex-1"></div>
+            </div>
+        </div>
     );
 }
 
