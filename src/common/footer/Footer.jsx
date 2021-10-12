f4a4ce4b4920c9f49d9a2a11aa4be1851a4ae708
JPig-Main <jumpei141107@gmail.com>
2021-10-12 13:38:14 +0900

    scss -> tailwind-css (footer)

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,7 +1,9 @@
-import './css/footer.css'
+// import './css/footer.css'
 
 const Footer = (props) => (
-    <footer>{`© ${props.site.created_by},　${props.site['created_at']['year']}`}</footer>
+    <footer className="h-10 text-center leading-10 bg-gray-500 text-white">
+        {`© ${props.site.created_by},　${props.site['created_at']['year']}`}
+    </footer>
 );
 
 export default Footer;
