35bbd8d20a41063fbaed56cf7de865a1d630cf5f
JPig-Main <jumpei141107@gmail.com>
2021-10-12 13:23:12 +0900

    scss -> tailwind-css (header)

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,10 +1,12 @@
 import Nav from "../nav/Nav"
-import "./css/header.css"
+// import "./css/header.css"
 
 const Header = (props) => (
-    <header>
-        <div className="title">{props.site.name}</div>
-        <Nav links={props.links}/>
+    <header className="h-16 bg-gray-700 text-gray-100 flex items-center">
+        <div className="w-24"></div>
+        <div className="flex-initial text-xl italic tracking-wider">{props.site.name}</div>
+        <div className="flex-1"></div>
+        <Nav links={props.links} className="flex-initial" />
     </header>
 );
 
