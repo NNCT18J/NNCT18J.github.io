19e54be7d69074f6384b86eae580dc7fe1b34e0b
JPig-Main <jumpei141107@gmail.com>
2021-10-12 13:46:34 +0900

    scss -> tailwind-css (page title)

--- src/components/pageTitle/PageTitle.jsx
+++ src/components/pageTitle/PageTitle.jsx
@@ -1,9 +1,13 @@
-import './css/pageTitle.css'
+// import './css/pageTitle.css'
+import './pageTitle.css'
 
 const PageTitle = (props) => (
-    <div className="title-container">
-        <div className="design">
-            <div className="contents">{props.title}</div>
+    <div className="title-container my-5 mx-auto">
+        <div className="design border-b-4 px-4">
+            <div 
+                className="contents text-4xl italic tracking-wider">
+                {props.title}
+            </div>
         </div>
     </div>
 )
